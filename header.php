5db5b9df49c0ec9477d5b36b9f6ee0580eb3fccb
DipuHaider <dipuhaider@gmail.com>
2019-02-17 03:26:24 +0600

    Update header.php

---
+++ header.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Tutorial theme</title>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/assets/js/jquery-3.3.1.js'; ?>">
+</script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/assets/js/jquery-ui.min.js'; ?>">
+</script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/assets/js/bootstrap.js'; ?>">
+</script>
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/assets/css/bootstrap.css'; ?>">
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+</head>
+
+<body>
+
+<div id="ttr_header" class="jumbotron">
+<h1>HEADER</h1>
+</div>
+<div class="container">
